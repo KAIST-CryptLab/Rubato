c4d48ebd129f31cceded56aeb7fca16b77990a1a
HaJincheol <smilecjf@naver.com>
2021-09-09 06:35:08 +0000

    Add randomized key schedule using XOF

--- Rubato.cpp
+++ Rubato.cpp
@@ -1,9 +1,12 @@
-#include "Rubato.h"
 #include <cstring>
 #include <cassert>
+#include <x86intrin.h>
+#include "Rubato.h"
 #ifndef NDBUG
 #include <iostream>
 #endif
+
+constexpr int unpack_order[8] = {2, 3, 6, 7, 0, 1, 4, 5};
 
 block_t block_init(size_t sz)
 {
@@ -43,15 +46,167 @@ void Rubato::get_rand_vectors(uint64_t *output)
 
 void Rubato::keyschedule()
 {
+	#if 0 // Naive rejection sampling
+	uint8_t buf[RATE_IN_BYTE];
+	unsigned int offset = 0;
+	unsigned int squeeze_byte = 0;
+
+	shake_->squeeze(buf, RATE_IN_BYTE);
+	int ctr = RATE_IN_BYTE / 4;
+	while (offset < XOF_ELEMENT_COUNT)
+	{
+		if (ctr == 0)
+		{
+			shake_->squeeze(buf, RATE_IN_BYTE);
+			squeeze_byte = 0;
+			ctr = RATE_IN_BYTE / 4;
+		}
+
+		uint64_t elem;
+		memcpy(&elem, buf + squeeze_byte, 4);
+		elem &= ((1ULL << MOD_BIT_COUNT) - 1);
+		squeeze_byte += 4;
+
+		if (elem < MODULUS)
+		{
+			rand_vectors_[offset] = elem;
+			offset++;
+		}
+		ctr--;
+	}
+
 	for (int r = 0; r <= ROUNDS; r++)
 	{
 		for (int i = 0; i < BLOCKSIZE; i++)
 		{
-			rand_vectors_[r * BLOCKSIZE + i] = (i + 1) % MODULUS;
 			round_keys_[r * BLOCKSIZE + i] = rand_vectors_[r * BLOCKSIZE + i] * key_[i];
 			round_keys_[r * BLOCKSIZE + i] %= MODULUS;
 		}
 	}
+	#else // Rejection sampleing using AVX2
+	// Set random vector from SHAKE256
+	const __m256i zero = _mm256_setzero_si256();
+	const __m256i modulus32 = _mm256_set1_epi32(MODULUS);
+	const __m256i all_one_mask32 = _mm256_set1_epi32((1ULL << MOD_BIT_COUNT) - 1);
+	__m256i u0, u1, u2, u3, v0, v1, v2, v3, idx;
+
+	__attribute__((aligned(32))) uint8_t buf[160];
+	memset(buf, 0xff, 160);
+
+	unsigned int offset = 0;
+	unsigned int squeeze_byte = 0;
+	__attribute__((aligned(32))) uint32_t idx_arr[8] = {0};
+
+	shake_->squeeze(buf, RATE_IN_BYTE);
+	int ctr = RATE_IN_BYTE / 32;
+	while (offset < XOF_ELEMENT_COUNT)
+	{
+		if (squeeze_byte == RATE_IN_BYTE)
+		{
+			shake_->squeeze(buf, RATE_IN_BYTE);
+			squeeze_byte = 0;
+			ctr = RATE_IN_BYTE / 32;
+		}
+
+		if (ctr > 0)
+		{
+			u0 = _mm256_loadu_si256((__m256i *)(buf + squeeze_byte));
+			u0 = _mm256_and_si256(u0, all_one_mask32);
+			u1 = _mm256_cmpgt_epi32(modulus32, u0);
+
+			int good_idx = _mm256_movemask_ps((__m256)u1);
+			int good_cnt = 0;
+			int bad_cnt = 0;
+			for (int i = 0; i < 8; i++)
+			{
+				if (good_idx & (1 << i))
+				{
+					idx_arr[unpack_order[good_cnt]] = i;
+					good_cnt++;
+				}
+				else
+				{
+					idx_arr[unpack_order[7 - bad_cnt]] = i;
+					bad_cnt++;
+				}
+			}
+			idx = _mm256_load_si256((__m256i *)idx_arr);
+			u2 = _mm256_permutevar8x32_epi32(u0, idx);
+
+			u0 = _mm256_unpackhi_epi32(u2, zero);
+			u1 = _mm256_unpacklo_epi32(u2, zero);
+			_mm256_storeu_si256((__m256i *)(rand_vectors_ + offset), u0);
+			_mm256_storeu_si256((__m256i *)(rand_vectors_ + offset + 4), u1);
+
+			offset += good_cnt;
+			squeeze_byte += 32;
+			ctr--;
+		}
+		else // Last 8 bytes in the squeezed bytes
+		{
+			u0 = _mm256_loadu_si256((__m256i *) (buf + squeeze_byte));
+			u0 = _mm256_and_si256(u0, all_one_mask32);
+			u1 = _mm256_cmpgt_epi32(modulus32, u0);
+
+			int good_idx = _mm256_movemask_ps((__m256)u1);
+			int good_cnt = 0;
+			int bad_cnt = 0;
+			for (int i = 0; i < 8; i++)
+			{
+				if (good_idx & (1 << i))
+				{
+					idx_arr[unpack_order[good_cnt]] = i;
+					good_cnt++;
+				}
+				else
+				{
+					idx_arr[unpack_order[7 - bad_cnt]] = i;
+					bad_cnt++;
+				}
+			}
+			idx = _mm256_load_si256((__m256i *)idx_arr);
+			u2 = _mm256_permutevar8x32_epi32(u0, idx);
+
+			u0 = _mm256_unpackhi_epi32(u2, zero);
+			u1 = _mm256_unpacklo_epi32(u2, zero);
+			_mm256_storeu_si256((__m256i *) (rand_vectors_ + offset), u0);
+
+			offset += good_cnt;
+			squeeze_byte += 8;
+		}
+	}
+
+	// Compute round keys
+	// round_keys[i] = (key_[i] * rand_vectors_[i]) % MODULUS;
+	u0 = _mm256_load_si256((__m256i *) key_);
+	u1 = _mm256_load_si256((__m256i *) (key_ + 4));
+	u2 = _mm256_load_si256((__m256i *) (key_ + 8));
+	u3 = _mm256_load_si256((__m256i *) (key_ + 12));
+
+	for (int  r = 0; r <= ROUNDS; r++)
+	{
+		v0 = _mm256_load_si256((__m256i *) (rand_vectors_ + BLOCKSIZE * r));
+		v1 = _mm256_load_si256((__m256i *) (rand_vectors_ + BLOCKSIZE * r + 4));
+		v2 = _mm256_load_si256((__m256i *) (rand_vectors_ + BLOCKSIZE * r + 8));
+		v3 = _mm256_load_si256((__m256i *) (rand_vectors_ + BLOCKSIZE * r + 12));
+
+		v0 = _mm256_mul_epu32(u0, v0);
+		v1 = _mm256_mul_epu32(u1, v1);
+		v2 = _mm256_mul_epu32(u2, v2);
+		v3 = _mm256_mul_epu32(u3, v3);
+
+		_mm256_store_si256((__m256i *) (round_keys_ + BLOCKSIZE * r), v0);
+		_mm256_store_si256((__m256i *) (round_keys_ + BLOCKSIZE * r + 4), v1);
+		_mm256_store_si256((__m256i *) (round_keys_ + BLOCKSIZE * r + 8), v2);
+		_mm256_store_si256((__m256i *) (round_keys_ + BLOCKSIZE * r + 12), v3);
+	}
+
+	for (int i = 0; i < (ROUNDS + 1) * BLOCKSIZE; i++)
+	{
+		round_keys_[i] %= MODULUS;
+	}
+
+	#endif
 }
 
 void Rubato::compute_keystream_naive(block_t out)
--- parms.h
+++ parms.h
@@ -4,8 +4,9 @@
 // Parameter
 #define BLOCKSIZE 16
 #define OUTPUTSIZE 12
-#define ROUNDS 10
-#define MODULUS 65537
+#define ROUNDS 4
+#define MODULUS 0x3ffc0001
+#define MOD_BIT_COUNT 30
 
 #define XOF_ELEMENT_COUNT ((ROUNDS + 1) * BLOCKSIZE)
 #endif
